5b08f62c8d5a32dedf464c08921746c454e6dcd6
SebasAure <saure@correo.unimet.edu.ve>
2024-01-28 01:12:13 -0400

    interfaces iniciales

--- Proyecto_1_SO_Aure_Perez/src/Clases/main.java
+++ Proyecto_1_SO_Aure_Perez/src/Clases/main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package Clases;
-
+import Interfaces.VentanaSimulacion;
 /**
  *
  * @author sebas
@@ -15,7 +15,8 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        VentanaSimulacion simulacion = new VentanaSimulacion();
+        simulacion.setVisible(true);
     }
     
 }
